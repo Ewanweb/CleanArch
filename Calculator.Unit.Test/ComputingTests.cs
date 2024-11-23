de12d42281429c377a321631c9daa580cda576ce
ماهان محمودی <MAHAN001MS@GMAIL.COM>
2024-11-23 17:20:23 +0330

    Add Test

---
+++ Calculator.Unit.Test/ComputingTests.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Unit.Test
+{
+    public class ComputingTests
+    {
+        [Fact(DisplayName ="Check Odd Calculator")]
+        public void OddOrEven_Should_Return_Odd_When_Input_Is_OddValue()
+        {
+            var computing = new Computing();
+
+            var result = computing.OddOrEven(11);
+
+            Assert.Equal("Odd", result);
+
+        }
+
+        [Theory(DisplayName = "Check Even Calculator")]
+        [InlineData(10)]
+        public void OddOrEven_Should_Return_Even_When_Input_Is_EvenValue(int value)
+        {
+            var computing = new Computing();
+
+            var result = computing.OddOrEven(value);
+
+            Assert.Equal("Even", result);
+
+        }
+    }
+}
---
+++ Calculator/Computing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Computing
+    {
+        public string OddOrEven(int value)
+        {
+            return value % 2 == 0 ? "Even" : "Odd";
+        }
+    }
+}
